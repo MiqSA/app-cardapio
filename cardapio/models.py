5d318e3daddd41284a6ba98c06fa18ddd8283309
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-11 11:50:25 -0300

    Adiciona tabela Prato ao projeto

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Prato(models.Model):
+    nome_prato = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    preco = models.FloatField()
+    rendimento = models.TextField(max_length=100)
+    categria = models.CharField(max_length=100)
+    date_prato = models.DateTimeField(default=datetime.now, blank=True)
