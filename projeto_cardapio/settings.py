aa5b61469c0fbc84061194b96231632a7d8e83e3
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-11 11:44:22 -0300

    Adiciona banco de dados postgresql

--- projeto_cardapio/settings.py
+++ projeto_cardapio/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'projeto_cardapio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '***',
+        'USER': '***',
+        'PASSWORD': '***',
+        'HOST': '***'
     }
 }
 
