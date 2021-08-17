145260faaeb5fb92602467a4c5761676c06c24dd
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-17 14:13:27 -0300

    Customiza admin

--- cardapio/admin.py
+++ cardapio/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Prato
 
-admin.site.register(Prato)
+
+class ListandoPratos(admin.ModelAdmin):
+    list_display = ('id', 'nome_prato', 'categoria')
+    list_display_links = ('id', 'nome_prato')
+    search_fields = ('nome_prato',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+
+
+admin.site.register(Prato, ListandoPratos)
