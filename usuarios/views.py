e9fe76f506bc2b25babd8c9c802e406e148d32c8
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-19 13:26:39 -0300

    Adiciona autenticação de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def cadastro(request):
@@ -36,14 +37,25 @@ def login(request):
             print('Email e senha não podem ficar em branco')
             return redirect('login')
         print(email, senha)
-        return redirect('dashboard')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request,user)
+                print('login realizado com sucesso')
+                return redirect('dashboard')
     else:
         return render(request, 'usuarios/login.html')
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
+
