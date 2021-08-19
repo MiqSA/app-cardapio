d8112fd6d7cd1dbd6ae5892b3c73ecdf35c42566
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-19 11:51:50 -0300

    Atualiza tratamento de cadastro e login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('Senha não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
-    return render(request, 'usuarios/login.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == '' or senha == '':
+            print('Email e senha não podem ficar em branco')
+            return redirect('login')
+        print(email, senha)
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/login.html')
 
 
 def logout(request):
@@ -14,4 +46,4 @@ def logout(request):
 
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
