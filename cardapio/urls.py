bc0a677947f07f903f89bde3f394b5424a2a769d
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-23 11:02:39 -0300

    Refatora views do cardapio e usuarios

--- cardapio/urls.py
+++ cardapio/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:prato_id>', views.cardapio, name='cardapio'),
-    path('buscar', views.buscar, name='buscar')
+    path('buscar', views.buscar, name='buscar'),
+    path('cria/prato', views.cria_prato, name='cria_prato'),
+    path('deleta/<int:prato_id>', views.deleta_prato, name='deleta_prato'),
+    path('edita/<int:prato_id>', views.edita_prato, name='edita_prato'),
+    path('atualiza_prato', views.atualiza_prato, name='atualiza_prato')
 ]
--- cardapio/views.py
+++ cardapio/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from .models import Prato
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
 
 def index(request):
@@ -27,4 +29,52 @@ def buscar(request):
             lista_pratos = lista_pratos.filter(nome_prato__icontains=nome_a_buscar)
 
     dados = {'pratos': lista_pratos}
-    return render(request, 'pratos/buscar.html', dados )
+    return render(request, 'pratos/buscar.html', dados)
+
+
+def cria_prato(request):
+    if request.method == 'POST':
+        nome_prato = request.POST['nome_prato']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        preco = request.POST['preco']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_prato = request.FILES['foto_prato']
+        user = get_object_or_404(User, pk=request.user.id)
+        prato = Prato.objects.create(pessoa=user, nome_prato=nome_prato, ingredientes=ingredientes,
+                                     modo_preparo=modo_preparo, preco=preco, rendimento=rendimento,
+                                     categoria=categoria, foto_prato=foto_prato)
+        prato.save()
+        return redirect('dashboard')
+    else:
+        return render(request, 'pratos/cria_prato.html')
+
+
+def edita_prato(request, prato_id):
+    prato = get_object_or_404(Prato, pk=prato_id)
+    prato_a_editar = {'prato': prato}
+    return render(request, 'pratos/edita_prato.html', prato_a_editar)
+
+
+def deleta_prato(request, prato_id):
+    prato = get_object_or_404(Prato, pk=prato_id)
+    prato.delete()
+    return redirect('dashboard')
+
+
+def atualiza_prato(request):
+    if request.method == 'POST':
+        prato_id = request.POST['prato_id']
+        p = Prato.objects.get(pk=prato_id)
+        p.nome_prato = request.POST['nome_prato']
+        p.ingredientes = request.POST['ingredientes']
+        p.modo_preparo = request.POST['modo_preparo']
+        p.preco = request.POST['preco']
+        p.rendimento = request.POST['rendimento']
+        p.categoria = request.POST['categoria']
+        if 'foto_prato' in request.FILES:
+            p.foto_prato = request.FILES['foto_prato']
+
+        p.save()
+        return redirect('dashboard')
